a30ccead9b6b93c7524fcabaa60a21e4930ab543
DorianYellow <dhtlq777@naver.com>
2024-10-03 17:45:31 +0900

    🐛 fix : Script File Doesn't Work #17 - Prompt has been modified.

--- setup_runpod.sh
+++ setup_runpod.sh
@@ -17,11 +17,12 @@ echo "FROM unsloth.Q5_K_M.gguf
 TEMPLATE \"\"\"<bos><start_of_turn>user
 Your role :
 - 당신은 특허 문서 작성을 도와주는 patent attorney 입니다.
-instructions :
+Instructions :
 - 특허 문서는 간결하고 명확하게 작성해야 합니다.
-- 청구항은 발명의 주요 구성 요소를 논리적 순서로 나열해야 합니다.
-- 동일하거나 구조적으로 매우 유사한 내용은 반복적으로 생성하지 말고 한 번만 명시해야 합니다.
-work :
+- 청구항 작성시 발명의 구성 요소및 특징만을 논리적 순서로 나열해야 합니다.
+- 청구항의 마지막 부분은 명사(noun)로 끝나야 합니다.
+- 동일하거나 구조적으로 유사한 문장은 반복하지 말고 한 번만 명시해야 합니다.
+Work :
 - 아래의 발명에 관한 설명을 바탕으로 '발명의 명칭'과 '청구항'을 특허 문서의 형식으로 작성해주세요.:
 {{.Prompt}}<end_of_turn>
 <start_of_turn>model\"\"\"
@@ -29,13 +30,13 @@ work :
 PARAMETER temperature 0
 PARAMETER num_predict 700
 PARAMETER num_ctx 4096
-PARAMETER repeat_penalty 1.15
+PARAMETER repeat_penalty 1.11
 PARAMETER stop <s>
 PARAMETER stop </s>
 " > Modelfile
 
 # Ollama 모델 생성
-ollama create test_model3_prompt3 -f Modelfile
+ollama create model_v0 -f Modelfile
 ollama list
 
 # 레포지토리 디렉토리로 이동
