b4b742c4ea1dab4954feec779f3af90e1fc451ca
DorianYellow <dhtlq777@naver.com>
2024-10-03 18:00:53 +0900

    💄 UI/UX : Add Example Input #11 - Added input examples for user convenience.

--- response_generator.py
+++ response_generator.py
@@ -3,7 +3,7 @@ import ollama
 
 def response_generator(prompt):
     """응답을 스트림 형태로 생성"""
-    response = ollama.generate(model="test_model3_prompt3", prompt=prompt, stream=True)
+    response = ollama.generate(model="model_v0", prompt=prompt, stream=True)
     for chunk in response:
         if chunk["response"] == "\n":
             chunk["response"] = "\n\n"
